5cfc60f3938175e2911d57b59d964ea9f6b22751
d1mznpc <dimasakuq@gmail.com>
2024-09-17 10:01:41 +0700

    menambahkan halaman buku tamu dan fitur menampilkan data tamu

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    include_once('template/header.php');
+?>
+
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+<!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Dashboard Page</h1>
+
+</div>
+<!-- /.container-fluid -->
+
+<?php
+    include_once('template/footer.php')
+?>
