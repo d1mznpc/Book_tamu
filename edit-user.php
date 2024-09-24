badb108c849f0f03e8b856918f7b35a55e32b6cf
d1mznpc <dimasakuq@gmail.com>
2024-09-24 09:23:18 +0700

    menambahkan fitur edit user

---
+++ edit-user.php
@@ -0,0 +1,81 @@
+<?php
+include_once('template/header.php');
+
+// Jika ada id_user di URL
+if (isset($_GET['id'])) {
+    $id_user = $_GET['id'];
+    // Ambil data user yang sesuai dengan id_user
+    $data = query("SELECT * FROM users WHERE id_user = '$id_user'")[0];
+}
+?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Ubah data user</h1>
+
+    <?php
+    if (isset($_POST['simpan'])) {
+        if (ubah_user($_POST) > 0) {
+    ?>
+            <div class="alert alert-success" role="alert">
+                Data berhasil diubah!
+            </div>
+        <?php
+        } else {
+        ?>
+            <div class="alert alert-danger" role="alert">
+                Data gagal diubah!
+            </div>
+    <?php
+        }
+    }
+    ?>
+
+    <!-- Konten edit data user -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6>Data User</h6>
+        </div>
+        <div class="card-body">
+            <form method="post" action="">
+                <input type="hidden" name="id_user" id="id_user" value="<?= $data['id_user'] ?>">
+                <div class="form-group row">
+                    <label for="username" class="col-sm-3 col-form-label">Username</label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="username" name="username" value="<?= $data['username'] ?>">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="user_role" class="col-sm-3 col-form-label">User Role</label>
+                    <div class="col-sm-8">
+                        <select class="form-control" name="user_role" id="user_role">
+                            <option value="admin" <?= $data['user_role'] == 'admin' ? 'selected' : ''; ?>>Administator</option>
+                            <option value="operator" <?= $data['user_role'] == 'operator' ? 'selected' : ''; ?>>Operator</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="" class="col-sm-3 col-form-label"></label>
+                    <div class="col-sm-8 d-flex justify-content-end">
+                        <div>
+                       <a type="button" class="btn btn-danger btn-icon-split" href="users.php">
+                        <span class="icon text-white-50">
+                            <i class="fas fa-chevron-left"></i>
+                        </span>
+                        <span class="text">Kembali</span>
+                        </a>
+                        <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+                    </div>
+                </div>
+        </div>  
+        </form>
+    </div>
+</div>
+</div>
+<!-- /.container-fluid -->
+
+<?php
+include_once('template/footer.php');
+?>
